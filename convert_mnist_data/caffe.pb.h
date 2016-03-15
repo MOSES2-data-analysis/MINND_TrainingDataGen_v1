c3a713cc930f59229554ba30e2648d0488c895ea
byrdie <roytsmart@gmail.com>
2016-03-14 20:23:09 -0600

    Added to see example of converting data into format accepted by Caffe

---
+++ convert_mnist_data/caffe.pb.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   caffe.pb.h
+ * Author: byrdie
+ *
+ * Created on March 14, 2016, 1:37 PM
+ */
+
+#ifndef CAFFE_PB_H
+#define CAFFE_PB_H
+
+
+
+#endif /* CAFFE_PB_H */
+
